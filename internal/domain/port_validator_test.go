ae0ffd4c92a778adfc75ceb9df08cfbe5276d952
TestardR <romain.rtestard@gmail.com>
2023-09-10 18:51:16 +0200

    refactor(code-review): refactor port_validator_test into table driven test.

--- internal/domain/port_validator_test.go
+++ internal/domain/port_validator_test.go
@@ -2,59 +2,64 @@ package domain
 
 import (
 	"errors"
-	"testing"
-
 	"github.com/onsi/gomega"
+	"testing"
 )
 
 func TestCanValidatePort(t *testing.T) {
 	t.Parallel()
 	g := gomega.NewWithT(t)
 
-	t.Run("with a valid port", func(t *testing.T) {
-		port := NewPort(
-			NewPortID("AEFJR"),
-			"Al Fujayrah",
-			"Al Fujayrah",
-			"United Arab Emirates",
-			[]string{},
-			[]string{},
-			NewCoordinates(25.12, 56.33),
-			"Ajman",
-			"Asia/Dubai",
-			[]string{"AEFJR"},
-			"",
-		)
+	portValidator := NewPortValidator()
 
-		portValidator := NewPortValidator()
+	tests := []struct {
+		description        string
+		port               Port
+		expectedViolations []error
+	}{
+		{
+			description: "with a valid port",
+			port: NewPort(
+				NewPortID("AEFJR"),
+				"Al Fujayrah",
+				"Al Fujayrah",
+				"United Arab Emirates",
+				[]string{},
+				[]string{},
+				NewCoordinates(25.12, 56.33),
+				"Ajman",
+				"Asia/Dubai",
+				[]string{"AEFJR"},
+				"",
+			),
+			expectedViolations: nil,
+		},
+		{
+			description: "with an invalid port",
+			port: NewPort(
+				NewPortID("AEFJR"),
+				"", // name should not be an empty string
+				"", // city should not be an empty string
+				"United Arab Emirates",
+				[]string{},
+				[]string{},
+				NewCoordinates(25.12, 0),
+				"Ajman",
+				"Asia/Dubai",
+				[]string{"AEFJR"},
+				"",
+			),
+			expectedViolations: []error{
+				errors.New("name is required"),
+				errors.New("city is required"),
+			},
+		},
+	}
 
-		violations := portValidator.Validate(port)
-		g.Expect(violations).To(gomega.BeNil())
-	})
-
-	t.Run("with an invalid port", func(t *testing.T) {
-		port := NewPort(
-			NewPortID("AEFJR"),
-			"", // name should not be an empty string
-			"", // city should not be an empty string
-			"United Arab Emirates",
-			[]string{},
-			[]string{},
-			NewCoordinates(25.12, 0),
-			"Ajman",
-			"Asia/Dubai",
-			[]string{"AEFJR"},
-			"",
-		)
-
-		portValidator := NewPortValidator()
-
-		expectedErrors := []error{
-			errors.New("name is required"),
-			errors.New("city is required"),
-		}
-
-		violations := portValidator.Validate(port)
-		g.Expect(violations).To(gomega.Equal(expectedErrors))
-	})
+	for _, tc := range tests {
+		t.Run(tc.description, func(t *testing.T) {
+			actualViolations := portValidator.Validate(tc.port)
+			g.Expect(actualViolations).To(gomega.Equal(tc.expectedViolations))
+		})
+	}
 }
